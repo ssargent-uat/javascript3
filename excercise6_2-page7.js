d0f3a2d3f844c55373f8a8ff4d6f91f5c6bd64fa
Stephen Sargent <uat@adminsnet.net>
2024-02-22 07:30:56 -0500

    Added excercise 6.2 Writing your own functions

---
+++ excercise6_2-page7.js
@@ -0,0 +1,50 @@
+//Module_main ()
+    //  Local Variables
+//    Declare Integer firstAge, secondAge, total
+
+    // Get the users age and the users best friends age
+//    Display "Enter your age."
+//    Input firstAge
+//    Display "Enter your best friends age."
+//   Input secondAge
+
+    // Get the sum of both ages.
+//    Set total = sum(firstAge, secondAge)
+
+    // Display the sum
+//    Display "Together you are  ", total, - years old."
+//End Module
+
+    // The sum function accepts two integer arguments and
+    // returns the sum of those arguments as an integer.
+//Function Integer sum(Integer num1, Integer num2)
+//    Declare Integer resullt
+//    Set result = num1 + num2
+//    Return result
+//End Function
+
+// Program Output
+// Enter your age
+// 22 Enter
+// Enter your best friends age.
+// 24 Enter
+// Together you are 46 years old
+
+Module_main ()
+        Declare Integer firstAge
+        Display "Enter your age."
+        Input firstAge
+    // secondAge part
+        Declare Integer secondAge
+        Display "Enter your best friends age."
+        Input  secondAge
+    // Get the sum
+        Set total = sum(firstAge, secondAge)
+        Display "Together you are  ", total, - " years old."
+    End Module 
+    
+    Function Integer sum(Integer num1, Integer num2)
+        Declare Integer resullt
+        Set result = num1 + num2
+        Return result
+    End Function
---
+++ excercise6_3.2-page3.js
@@ -0,0 +1,8 @@
+// The code for a module is known as a module definition.
+    Module showMessage()
+        Display "Hello World."
+      End Module
+      
+// To execute the module you write a statement that calls it
+        Call showMessage()
+        
