c3f3415e5bbc3f086b5c7bf7ff90e54bf33c9908
Stephen Sargent <uat@adminsnet.net>
2024-02-22 14:41:04 -0500

    added function to file.js

--- file.js
+++ file.js
@@ -27,3 +27,33 @@ var ageOfPerson = 20;
 if(ageOfPerson >= 18){
     console.log("You are andult!")
 }
+
+
+//Sample game
+var dice1 = 2;
+var dice2 = 5;
+
+var sum = dice1 + dice2;
+console.log(sum);
+
+if (sum == 7 || sum == 11){
+    console.log("you won");
+}
+else if(dice1 == dice2 && dice1%dice2 == 0){
+    console.log("Try Again !");
+}
+
+
+
+// Functions
+function greeting(fname){
+    console.log("Hello" + fname);
+}
+greeting("jael");
+greeting("Adam");
+
+function add(a,b){
+    return a+b;
+}
+let result = add(3,4);
+console.log(result);
