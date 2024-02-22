6fda7d1995647b767ce59b6f4532604205d4be13
Stephen Sargent <uat@adminsnet.net>
2024-02-22 07:39:48 -0500

    Excercise  3.4 passing arguments to modules

--- excercise6_3.2-page3.js
+++ excercise6_3.2-page3.js
@@ -5,4 +5,15 @@
       
 // To execute the module you write a statement that calls it
         Call showMessage()
-        
+
+// Excercise  3.4 passing arguments to modules
+    Module main()
+    Display "The sum of 12 and 45 is"
+    Call showSum(12, 45)
+End Module
+
+Module showSum(Integer num1, Integer num2)
+    Declare Integer result
+    Set result = num1 + num2
+    Display result
+End Module
